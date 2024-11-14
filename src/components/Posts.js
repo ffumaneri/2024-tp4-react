21bf5b87d5f2137215c45d40794ed88ca25c18bd
MicaCaceres <mica.caceres@live.com>
2024-11-14 16:36:52 -0300

    Tp 4- Finalizado

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,40 +3,57 @@ import { useEffect, useState } from "react";
 import { Container, Spinner } from "react-bootstrap";
 
 function Posts() {
-    
-    //TODO: Agregar los states necesarios
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [loading, setLoading] = useState(true)
- 
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then((response) => {
-                // TODO: leer los posts
-
-            })
-    }, [])
-
-    const showPosts = () => {
-        return(
-            {/* TODO: Mostrar tabla de posts*/ }
-        ) 
-    }
-    if (loading) {
-        return (
-            <Container>
-                <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-                </Spinner> 
-                Cargando... (acá van a estar los posts)
-            </Container>
-          );        
-    }
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      setPosts(response.data);
+      setLoading(false);
+    });
+  }, []);
+  const postsContent = () => {
+    const content = posts.map((post) => (
+      <tr>
+        <td>{post.userId}</td>
+        <td>{post.id}</td>
+        <td>{post.title}</td>
+        <td>{post.body}</td>
+      </tr>
+    ));
+    return <tbody>{content}</tbody>;
+  };
+  const showPosts = () => {
     return (
-        <Container>
-            <h1>Posts</h1>
-            {/* TODO: mostrar POSTS */}
-        </Container>
-    )
+      <table>
+        <thead>
+          <tr>
+            <th>UserId</th>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        {postsContent()}
+      </table>
+    );
+  };
+  if (loading) {
+    return (
+      <Container>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+        Cargando... (acá van a estar los posts)
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <h1>Posts</h1>
+      {posts != null && showPosts()}
+    </Container>
+  );
 }
 
 export default Posts;
