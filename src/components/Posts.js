581a543ca4e610dd13c382fb490246eab90ea0ba
Santiago Castro <santiiago71089@gmail.com>
2024-11-15 01:44:51 -0300

    TP4-Terminado

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Button, Container, Spinner, Table } from "react-bootstrap";
 
 function Posts() {
     
-    //TODO: Agregar los states necesarios
-
+    const [posts, setPosts] = useState(null)
     const [loading, setLoading] = useState(true)
  
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
-                // TODO: leer los posts
-
+                setPosts(response.data)
+                setLoading(false)
             })
     }, [])
 
+    const postsContent = () => {
+        const content = posts.map(post =>
+            <tr>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+                <td><Button href={'/edituser?id=' + post.id}>Editar</Button></td>
+            </tr>
+        )
+        return <tbody>{content}</tbody>
+    }
+
     const showPosts = () => {
         return(
-            {/* TODO: Mostrar tabla de posts*/ }
+            <Table class="table table-danger table-striped table-bordered">
+                <thead class="table-danger">
+                    <tr>
+                        <th scope="col" style={{with:'75px'}}>userId</th>
+                        <th scope="col">id</th>
+                        <th scope="col">title</th>
+                        <th scope="col">body</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                {postsContent()}
+            </Table>
         ) 
     }
     if (loading) {
@@ -27,14 +50,15 @@ function Posts() {
                 <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
                 </Spinner> 
-                Cargando... (acá van a estar los posts)
+                Cargando...
             </Container>
           );        
     }
     return (
         <Container>
             <h1>Posts</h1>
-            {/* TODO: mostrar POSTS */}
+            {posts != null && showPosts()}
+            <Button href="/adduser">Agregar Post</Button>
         </Container>
     )
 }
