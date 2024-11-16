86d5309856379a45a255e2eaa46c889716e013d6
GrimaldiKevin <104236678+GrimaldiKevin@users.noreply.github.com>
2024-11-16 08:41:25 -0300

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,19 +6,45 @@ function Posts() {
     
     //TODO: Agregar los states necesarios
 
+    const [posts, setPosts] = useState(null)
     const [loading, setLoading] = useState(true)
  
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
                 // TODO: leer los posts
-
+                setPosts(response.data)
+                setLoading(false)
             })
     }, [])
 
+    const postsContent = () => {
+        const content = posts.map(post =>
+            <tr>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+                <td><Button href={'/editpost?id=' + post.id}>Editar</Button></td>
+            </tr>
+        )
+        return <tbody>{content}</tbody>
+    }
+
     const showPosts = () => {
         return(
-            {/* TODO: Mostrar tabla de posts*/ }
+            /*{ TODO: Mostrar tabla de posts }*/
+            <Table>
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                {postsContent()}
+            </Table>
         ) 
     }
     if (loading) {
@@ -35,6 +61,8 @@ function Posts() {
         <Container>
             <h1>Posts</h1>
             {/* TODO: mostrar POSTS */}
+            {posts != null && showPosts()}
+            <Button href="/addpost">Agregar Post</Button>
         </Container>
     )
 }
