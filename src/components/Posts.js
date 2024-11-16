65427bb59cf3771e953fa025f85a75c36a1e1959
GastonBrondani <gaston.brondani@gmail.com>
2024-11-16 14:16:47 -0300

    e

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,17 +3,17 @@ import { useEffect, useState } from "react";
 import { Container, Spinner } from "react-bootstrap";
 
 function Posts() {
-    
+
     //TODO: Agregar los states necesarios
 
     const [loading, setLoading] = useState(true)
- 
+
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
                 // TODO: leer los posts
                 setUsers(response.data)
-                setLoading(false)  
+                setLoading(false)
 
 
             })
@@ -21,42 +21,42 @@ function Posts() {
 
     const postsContent = () => {
         const content = posts.map((post) => (
-          <tr>
+            <tr>
 
-            <td>{post.userId}</td>
-            <td>{post.id}</td>
-            <td>{post.title}</td>
-            <td>{post.body}</td>
-          </tr>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>
         ));
         return <tbody>{content}</tbody>;
-      };
+    };
 
-      const showPosts = () => {
+    const showPosts = () => {
         return (
-          <table>
-            <thead>
-              <tr>
-                <th>UserID</th>
-                <th>Id</th>
-                <th>Title</th>
-                <th>Body</th>
-              </tr>
-            </thead>
-            {postsContent()}
-          </table>
+            <table>
+                <thead>
+                <tr>
+                    <th>UserID</th>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                </tr>
+                </thead>
+                {postsContent()}
+            </table>
         );
-      };
+    };
 
     if (loading) {
         return (
             <Container>
                 <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-                </Spinner> 
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
                 Cargando... (acá van a estar los posts)
             </Container>
-          );        
+        );
     }
     return (
         <Container>
