ba7094d607d5b576fb7d24edd21ba8018b965c84
florenciamj01 <florenciamj01@gmail.com>
2024-11-18 19:53:56 -0300

    TP4

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,25 +1,53 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Container, Spinner, Table } from "react-bootstrap";
 
 function Posts() {
     
     //TODO: Agregar los states necesarios
 
+    const [posts, setPosts] = useState(null)
     const [loading, setLoading] = useState(true)
- 
+
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
                 // TODO: leer los posts
 
+                setPosts(response.data)
+                setLoading(false)
             })
     }, [])
 
+    const postContent = () => {
+        const content = posts.map(post =>
+            <tr>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+                {/* <td><Button href={'/edituser?id=' + user.id}>Editar</Button></td> */}
+            </tr>
+        )
+        return <tbody>{content}</tbody>
+    }
+
     const showPosts = () => {
+        {/* TODO: Mostrar tabla de posts*/ }
         return(
-            {/* TODO: Mostrar tabla de posts*/ }
+            <Table>
+                <thead>
+                    <tr>
+                        <th>User Id</th>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>body</th>
+                    </tr>
+                </thead>
+                {postContent()}
+            </Table>
         ) 
+        
     }
     if (loading) {
         return (
@@ -29,12 +57,12 @@ function Posts() {
                 </Spinner> 
                 Cargando... (acá van a estar los posts)
             </Container>
-          );        
+        );        
     }
     return (
         <Container>
             <h1>Posts</h1>
-            {/* TODO: mostrar POSTS */}
+            {showPosts()}
         </Container>
     )
 }
