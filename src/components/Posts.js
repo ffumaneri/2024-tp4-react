fdf6d25ee1d3f6c460580d1ddb391150f3c23ff2
Augusto <augustobelini007@gmail.com>
2024-11-18 17:20:42 -0300

    Tp4-Sprint-1

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Container, Spinner, Table, Button } from "react-bootstrap";
 
 function Posts() {
     
     //TODO: Agregar los states necesarios
-
+    const [posts, setPosts] = useState(null);
     const [loading, setLoading] = useState(true)
  
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
                 // TODO: leer los posts
-
+                setPosts(response.data);
+                setLoading(false);
             })
     }, [])
 
+    const postsContent = () => {
+        const content = posts.map(post => (
+            <tr key={post.id}>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>
+        ));
+        return <tbody>{content}</tbody>;
+    };
+
     const showPosts = () => {
         return(
-            {/* TODO: Mostrar tabla de posts*/ }
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Post ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                {postsContent()}
+            </Table>
         ) 
     }
     if (loading) {
@@ -35,6 +58,7 @@ function Posts() {
         <Container>
             <h1>Posts</h1>
             {/* TODO: mostrar POSTS */}
+            {showPosts()}
         </Container>
     )
 }
