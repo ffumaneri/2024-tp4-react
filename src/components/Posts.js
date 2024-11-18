5431c2e86f003d989e0ac3acae6904c2811a0169
wNot <exequielvittor@hotmail.com>
2024-11-18 19:53:18 -0300

    tp4

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,49 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Container, Spinner, Table  } from "react-bootstrap";
 
 function Posts() {
     
     //TODO: Agregar los states necesarios
-
+    const [posts, setPost] = useState(null)
     const [loading, setLoading] = useState(true)
  
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
-                // TODO: leer los posts
+                
+                
+                setPost(response.data)
+                setLoading(false)
 
             })
     }, [])
 
+    const postContent = () => {
+        const postCont = posts.map(posts =>
+            <tr>
+                <td>{posts.userId}</td>
+                <td>{posts.id}</td>
+                <td>{posts.title}</td>
+                <td>{posts.body}</td>
+            </tr>
+        )
+        return <tbody>{postCont}</tbody>
+    
+    }
     const showPosts = () => {
         return(
-            {/* TODO: Mostrar tabla de posts*/ }
+            <Table>
+                <thead>
+                    <tr>
+                        <th>UserID</th>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                {postContent()}
+            </Table>
         ) 
     }
     if (loading) {
@@ -27,14 +52,14 @@ function Posts() {
                 <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
                 </Spinner> 
-                Cargando... (acá van a estar los posts)
+                Cargando Posts...
             </Container>
           );        
     }
     return (
         <Container>
             <h1>Posts</h1>
-            {/* TODO: mostrar POSTS */}
+            {posts != null && showPosts()}
         </Container>
     )
 }
