7dc7634e4a1d11adedaff4ebe2ab2828e614b4cf
euurodri <111576327+euurodri@users.noreply.github.com>
2024-11-19 16:59:36 -0300

    Revert "Tp"

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,65 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Container, Spinner, Table } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 
 function Posts() {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
+    
+    //TODO: Agregar los states necesarios
 
+    const [loading, setLoading] = useState(true)
+ 
     useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-                setPosts(response.data);
-            } catch (error) {
-                console.error("Error fetching posts:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchPosts();
-    }, []);
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                // TODO: leer los posts
 
-    const renderPostsTable = () => (
-        <Table striped bordered hover responsive>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>User ID</th>
-                    <th>Title</th>
-                    <th>Body</th>
-                </tr>
-            </thead>
-            <tbody>
-                {posts.map((post) => (
-                    <tr key={post.id}>
-                        <td>{post.id}</td>
-                        <td>{post.userId}</td>
-                        <td>{post.title}</td>
-                        <td>{post.body}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
-    );
+            })
+    }, [])
 
+    const showPosts = () => {
+        return(
+            {/* TODO: Mostrar tabla de posts*/ }
+        ) 
+    }
     if (loading) {
         return (
-            <Container className="text-center mt-5">
+            <Container>
                 <Spinner animation="border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </Spinner>
-                <p>Cargando... Por favor espera.</p>
+                <span className="visually-hidden">Loading...</span>
+                </Spinner> 
+                Cargando... (acá van a estar los posts)
             </Container>
-        );
+          );        
     }
-
     return (
-        <Container className="mt-5">
-            <h1 className="mb-4">Posts</h1>
-            {posts.length > 0 ? renderPostsTable() : <p>No se encontraron posts.</p>}
+        <Container>
+            <h1>Posts</h1>
+            {/* TODO: mostrar POSTS */}
         </Container>
-    );
+    )
 }
 
 export default Posts;
