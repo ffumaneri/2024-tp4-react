620ae81af769d71a64861938ba4a26eaebcdb280
Lautaro Waigand <lautaro.waigand@outlook.com>
2024-11-20 20:56:50 -0300

    commitito

---
+++ src/components/Post.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Spinner, Table } from "react-bootstrap";
+
+function Posts() {
+    
+    //TODO: Agregar los states necesarios
+    const [posts, setPosts] = useState(null);
+    const [loading, setLoading] = useState(true)
+ 
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                // TODO: leer los posts
+            setPosts(response.data);
+            setLoading(false);
+            })
+    }, [])
+
+    const postsContent = () => {
+        const content = posts.map(post => (
+            <tr key={post.id}>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>
+        ));
+        return <tbody>{content}</tbody>;
+    }; 
+
+    const showPosts = () => {
+        return(
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Post ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                {postsContent()}
+            </Table>
+        ) 
+    }
+
+    if (loading) {
+        return (
+            <Container>
+                <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+                </Spinner> 
+                Cargando... (aca van a estar los posts)
+            </Container>
+          );        
+    }
+    return (
+        <Container>
+            <h1>Posts</h1>
+            {/* TODO: mostrar POSTS */}
+            {posts!=null && showPosts()}
+        </Container>
+    )
+}
+
+export default Posts;
